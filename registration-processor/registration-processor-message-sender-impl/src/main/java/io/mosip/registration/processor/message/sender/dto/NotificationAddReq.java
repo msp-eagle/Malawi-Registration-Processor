d429a022c145026ff76bc876ecc171a919abf539
msp-eagle <msp.philsys@gmail.com>
2023-11-02 09:59:39 +0530

    update fix

---
+++ registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/dto/NotificationAddReq.java
@@ -0,0 +1,17 @@
+package com.msp.philsys.recordhistory.Dto.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationAddReq {
+
+    private String notificationType;
+    private String notificationId;
+    private String requestServiceName;
+    private String status;
+
+}
---
+++ registration-processor/registration-processor-message-sender-impl/src/main/java/io/mosip/registration/processor/message/sender/dto/NotificationAddResponse.java
@@ -0,0 +1,12 @@
+package com.msp.philsys.recordhistory.Dto.notification;
+
+
+import lombok.Data;
+
+@Data
+public class NotificationAddResponse {
+
+    private String statusCode;
+    private String statusMessage;
+
+}
